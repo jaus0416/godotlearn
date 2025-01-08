ba744b9e9dc2a6116f8ca3a82aa09e6ef723a5b7
jaus0416_mac <andrew_yu0416@gmail.com>
2025-01-08 21:05:30 +0800

    feature:动作冒险rpg游戏-小问题优化（月球漫步、hurtbox位置调整）

--- 05_2d_adventure_rpg_game/player/player.gd
+++ 05_2d_adventure_rpg_game/player/player.gd
@@ -3,6 +3,7 @@ class_name Player
 
 var cardinal_direction : Vector2 = Vector2.DOWN
 var direction : Vector2 = Vector2.ZERO
+const DIR_4 = [Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT, Vector2.UP]
 
 signal DirectionChanged(new_direction : Vector2)
 
@@ -25,14 +26,10 @@ func _physics_process(_delta: float) -> void:
 	pass
  
 func set_direction() -> bool:
-	var new_direction : Vector2 = cardinal_direction
 	if direction == Vector2.ZERO:
 		return false
-	
-	if direction.y == 0:
-		new_direction = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
-	elif direction.x == 0:
-		new_direction = Vector2.UP if direction.y < 0 else Vector2.DOWN
+	var direction_id : int = int(round((direction + cardinal_direction * 0.1).angle() / TAU * DIR_4.size()))
+	var new_direction = DIR_4[direction_id]
 	
 	if new_direction == cardinal_direction:
 		return false
--- 05_2d_adventure_rpg_game/player/player_state_attack.gd
+++ 05_2d_adventure_rpg_game/player/player_state_attack.gd
@@ -11,7 +11,7 @@ var attacking : bool = false
 @onready var walk : PlayerState = $"../Walk"
 @onready var attack_anim : AnimationPlayer = $"../../Sprite2D/AttackEffectedSprite/AnimationPlayer"
 @onready var audio : AudioStreamPlayer2D = $"../../Audio/AudioStreamPlayer2D"
-@onready var hurt_box : HurtBox = $"../../Interactions/HurtBox"
+@onready var hurt_box : HurtBox = %AttackHurtBox
 
 func enter() -> void:
 	# animation
