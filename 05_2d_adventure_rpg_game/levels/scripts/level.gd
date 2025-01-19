d33599d3a3e1a2ebce59fcb34cb6e0acea11ecc5
jaus0416_mac <andrew_yu0416@gmail.com>
2025-01-19 21:11:35 +0800

    feature:动作冒险rpg游戏-添加全局背景音管理器

--- 05_2d_adventure_rpg_game/levels/scripts/level.gd
+++ 05_2d_adventure_rpg_game/levels/scripts/level.gd
@@ -1,10 +1,13 @@
 extends Node2D
 class_name Level
 
+@export var music : AudioStream
+
 func _ready() -> void:
 	self.y_sort_enabled = true
 	PlayerManager.set_as_parent(self)
 	LevelManager.level_load_started.connect(_free_level)
+	AudioManager.play_music(music)
 	pass
 
 func _free_level() -> void:
